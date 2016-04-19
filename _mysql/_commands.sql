f3cc3d46ab2643720c28571dcbe0bb93f52b3a6f
splinter <splinta89@gmail.com>
2016-04-19 12:23:56 -0700

    more commands

--- _mysql/_commands.sql
+++ _mysql/_commands.sql
@@ -33,3 +33,25 @@ ALTER TABLE `accommodation` AUTO_INCREMENT = 1;
 ALTER TABLE `user` AUTO_INCREMENT = 1;
 
 C:\wamp\bin\mysql\mysql5.7.9\bin\mysqlbinlog -v --base64-output=DECODE-ROWS C:\wamp\bin\mysql\replication_project\data_master\mysql-bin.000017
+
+STOP SLAVE;
+STOP SLAVE IO_THREAD;
+STOP SLAVE SQL_THREAD;
+START SLAVE;
+START SLAVE IO_THREAD;
+START SLAVE SQL_THREAD;
+
+
+
+-- Multi-master
+C:\wamp\bin\mysql\mysql5.7.9\bin\mysqld --initialize-insecure --datadir=C:\wamp\bin\mysql\replication_project_multi\data_master1
+C:\wamp\bin\mysql\mysql5.7.9\bin\mysqld --initialize-insecure --datadir=C:\wamp\bin\mysql\replication_project_multi\data_slave
+C:\wamp\bin\mysql\mysql5.7.9\bin\mysqld --defaults-file=C:\wamp\bin\mysql\replication_project_multi\master1.ini --master-info-repository=TABLE --relay-log-info-repository=TABLE
+C:\wamp\bin\mysql\mysql5.7.9\bin\mysqld --defaults-file=C:\wamp\bin\mysql\replication_project_multi\slave.ini --master-info-repository=TABLE --relay-log-info-repository=TABLE
+-- create user
+C:\wamp\bin\mysql\mysql5.7.9\bin\mysqldump -uroot --port=3306 --all-databases --master-data > C:\wamp\bin\mysql\replication_project_multi\master_dump.sql
+
+-- SLAVE:
+CHANGE MASTER TO MASTER_HOST='localhost', MASTER_PORT=3306, MASTER_USER='repl', MASTER_PASSWORD='pass', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=611 FOR CHANNEL 'master1';
+SELECT * FROM mysql.slave_master_info;
+
