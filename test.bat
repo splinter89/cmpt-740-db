1387817d610eb9f8d3866336aee28b760546f31d
splinter <splinta89@gmail.com>
2016-04-18 18:00:32 -0700

    rename some

--- test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-for /L %%i in (1,1,1000) do (cls && echo Iteration: %%i/1000 && echo. && C:\wamp\bin\apache\apache2.4.17\bin\ab -n 100 -c 100 740project.lc/add_random.php && timeout /t 2 /nobreak >nul)
-pause
