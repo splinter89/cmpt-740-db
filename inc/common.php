c9f6f1061bda19a1e2804fb16b9225fae018d38e
splinter <splinta89@gmail.com>
2019-01-08 15:50:40 -0800

    fix undefined const

--- inc/common.php
+++ inc/common.php
@@ -10,3 +10,4 @@ Log::setUp(ini_get('error_log'));
 DB::setUp(require 'db_config.php', 'test_server');
 
 define('READ_DB_CONNECTION_NAME', chooseConnectionName(true));
+define('WRITE_DB_CONNECTION_NAME', chooseConnectionName(false, $_GET['accommodation']['city']));
