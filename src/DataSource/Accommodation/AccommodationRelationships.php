7a9d15f1e841ec4c82ef2dbdafd6b8ed90cf790a
splinter <splinta89@gmail.com>
2019-03-03 23:14:06 -0800

    configure relationships

--- src/DataSource/Accommodation/AccommodationRelationships.php
+++ src/DataSource/Accommodation/AccommodationRelationships.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace App\DataSource\Accommodation;
 
+use App\DataSource\User\User;
 use Atlas\Mapper\MapperRelationships;
 
 class AccommodationRelationships extends MapperRelationships
 {
     protected function define()
     {
+        $this->manyToOne('host_user', User::class, [
+            'host_user_id' => 'id',
+        ]);
     }
 }
